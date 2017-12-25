45c646f0c7c33006111ffd88c43ca04d61eee42b
dustin <dustinerickson@gmail.com>
2017-12-25 00:46:05 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+"/views/partials");
+app.set("view_engine",'hbs');
+
+//app.use(function(req, resp, next){
+//     var data = {
+//        title:"Maintenance Page",
+//        body:"Page needed some attention."
+//    }
+//    resp.render('maintenance.hbs', data);
+//});
+app.use( function(req,resp,next){
+    var now = new Date().toDateString();
+    var reqUrl= req.url;
+    var reqMethod = req.method;
+    var log = `now : ${now}, req URL : ${reqUrl}, Method : ${reqMethod}`;
+   
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", function(err){
+        if(err){
+            console.log("unable to append to server.log");
+        }
+    })
+    next();
+});
+
+app.use(express.static(__dirname+"/public/"));
+
+
+hbs.registerHelper("getCurrentYear", function(){
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", function(text){
+    return text.toUpperCase();
+});
+
+app.get("/", function(req,resp){
+    var data = {
+        title:"Home Page",
+        body:"boner"
+    }
+   resp.render('home.hbs', data);
+})
+
+app.get("/about", function(req, resp){
+    var data = {
+        title:"About Page",
+        body:"party time in the osakrs Brichs"
+    }
+   resp.render('about.hbs', data);
+});
+
+app.get("/bad", function(req, resp){
+    resp.send({
+        error:"Bad link, hombre~"
+    })
+})
+
+app.listen(8000,function(){
+    console.log("server listening on post 8000");
+});
